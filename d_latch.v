4684b72ee5f3ceed1e724d0b44d5098286cfcbd4
mads-88 <madisonhickey@u.boisestate.edu>
2026-03-24 15:38:34 -0600

    Create d_latch.v

---
+++ d_latch.v
@@ -0,0 +1,15 @@
+module d_latch(
+    input D,
+    input E,
+    output reg Q,
+    output NotQ
+);
+
+    always @(D, E) begin
+        if (E)
+            Q <= D;
+    end
+
+    assign NotQ = ~Q;
+
+endmodule
